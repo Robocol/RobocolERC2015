8f178f8791e5698c6eb01cf6092ed15c35a0531f
gf.giraldo239@uniandes.edu.co <german@german.(none)>
2015-04-17 20:20:37 -0500

    Makefile y compilados de lib y prueba para ADC. Actualización de prueba_stepper.c

--- Code/Galileo/Perifericos/STEPPER/prueba_stepper.c
+++ Code/Galileo/Perifericos/STEPPER/prueba_stepper.c
@@ -24,20 +24,20 @@ devptr=&dev1;
 
 
 		printf("Bienvenido al test de funcionamiento de Puente H (ERC 2015-ROBOCOL).\n Utilice una de los siguientes comandos:\n" 
-				"\t enable\t\t\t-Habilita el puente H\n"
-				"\t disable\t\t\t-Deshabilita el puente H\n"
-				"\t medir-velocidad\t-Imprime la velocidad actual del motor\n "
-				"\t medir-temperatura\t-Imprime la temperatura actual del motor\n "
-				"\t medir-corriente\t-Imprime la corriente actual que atravieza el motor\n"
-				"\t dar-estado\t \t-Imprime el estado de operación actual del motor\n"
-				"\t cambiar-pwm\t\t-Cambia ciclo útil del PWM. Valor de entrada entre 0 y 255\n"
-				"\t cambiar-direccion\t-Cambia la dirección de giro del motor. Valor de entrada 1 o 0\n"
-				"\t cambiar-velocidad\t-Cambia la velocidad objetivo del controlador. Valor de entrada entre ------\n"
-				"\t cambiar-estado\t \t-Cambia el estado de operación del puente H. VAlor de entrada entre 16 o 32 TODO: Parser\n"
-				"\t cambiar-kic\t\t-Cambia el valor de KIC. Valor de entrada entre 0 y 255\n"
-				"\t cambiar-kpc\t\t-Cambia el valor de KPC. Valor de entrada entre 0 y 255\n"
-				"\t cambiar-kiv\t\t-Cambia el valor de KIV. Valor de entrada entre 0 y 255\n"
-				"\t cambiar-kpv\t\t-Cambia el valor de KPV. Valor de entrada entre 0 y 255\n"
+				"\t enable\t\t\t-Habilita el stepper\n"
+				"\t disable\t\t\t-Deshabilita el stepper\n"
+				"\t stat\t\t\t\t-Imprime el STATUS register\n "
+				"\t get-config\t\t\t-Imprime el CONFIG register\n "
+				"\t get-step\t\t\t-Imprime el estado actual del selector de paso\n"
+				"\t get-alarm\t\t\t-Imprime el registro de alarmas\n"
+				"\t get-ocdt\t\t\t-Imprime el Overcurrent Detection Threshold actual\n"
+				"\t get-tval\t\t\t-Retorna TVAL\n"
+				"\t periodo\t\t\t-Cambia el periodo del step\n"
+				"\t set-config\t\t\t-Imprime el CONFIG register\n "
+				"\t set-step\t\t\t-Imprime el estado actual del selector de paso\n"
+				"\t set-alarm\t\t\t-Imprime el registro de alarmas\n"
+				"\t set-ocdt\t\t\t-Imprime el Overcurrent Detection Threshold actual\n"
+				"\t set-tval\t\t\t-Imprime el TVAL\n"
 				"\t debug\t\t\t-Permite rápido envia de CAMBIAR_PWM con un valor de 0x5A\n");
 
 while(1){
@@ -57,89 +57,93 @@ while(1){
 
 		}else if(!strcmp(line,"get-alarm\n")){
 			stp_getAlarmEn(devptr,&alarm);
-			printf("Alarm: %d \n",alarm);
+			printf("Alarm: %X \n",alarm);
 
 		}else if(!strcmp(line,"get-step\n")){
 			stp_getStepSel(devptr,&step);
 			printf("Step: %X \n",step);
+
+		}else if(!strcmp(line,"get-posicion\n")){
+			stp_getPosition(devptr,&step);
+			printf("Posición: %X \n",step);
 
 		}else if(!strcmp(line,"get-ocdt\n")){
 			stp_getOCDT(devptr,&ocd);
 			printf("Estado: %d \n",est);
 		
 		//Setters
-		}else if(!strcmp(line,"pwm\n")){
+		}else if(!strcmp(line,"periodo\n")){
 			printf("Ingrese el periodo del paso en ns:\n");
 			getline(&line,&size,stdin);
 			buf=atoi(line);
-			printf("Cambiando PWM a: %d \n",buf);
-			stp_setStepPeriod(devptr,buf);
+			printf("Cambiando periodo a: %d \n",buf);
+			stp_period(devptr,buf);
  
 		}else if(!strcmp(line,"set_posicion\n")){
 			printf("Ingrese el marcador de la posición actual:\n");
 			getline(&line,&size,stdin);
 			buf=atoi(line);
 			printf("Cambiando posición a: %d \n",buf);
-			ph_setDireccion(devptr,buf);
+			stp_setPosition(devptr,buf);
 
 		}else if(!strcmp(line,"set-tval\n")){
 			printf("Ingrese el TVAL deseado:\n");
 			getline(&line,&size,stdin);
 			buf=atoi(line);
 			printf("Cambiando TVAL a: %d \n",buf);
-			ph_setTVal(devptr,buf);
+			stp_setTVal(devptr,buf);
 
 		}else if(!strcmp(line,"set-tonmin\n")){
 			printf("Ingrese el TONMIN deseado:\n");
 			getline(&line,&size,stdin);
 			buf=atoi(line);
 			printf("Cambiando TONMIN a: %d \n",buf);
-			ph_setTOnMin(devptr,buf);
+			stp_setTOnMin(devptr,buf);
 
 		}else if(!strcmp(line,"set-toffmin\n")){
 			printf("Ingrese el TOFFMIN deseado:\n");
 			getline(&line,&size,stdin);
 			buf=atoi(line);
 			printf("Cambiando TOFFMIN a: %d \n",buf);
-			ph_setOffMin(devptr,buf);
+			stp_setOffMin(devptr,buf);
 
 		}else if(!strcmp(line,"set-ocdt\n")){
 			printf("Ingrese el OCDT deseado:\n");
 			getline(&line,&size,stdin);
 			buf=atoi(line);
 			printf("Cambiando OCDT a: %d \n",buf);
-			ph_setOCDT(devptr,buf);
+			stp_setOCDT(devptr,buf);
 
 		}else if(!strcmp(line,"enable\n")){
-			printf("Ingrese 1 para activar, 0 para desactivar:\n");
-			getline(&line,&size,stdin);
-			buf=atoi(line);
-			printf("Cambiando enable a: %d \n",buf);
-			if(buf){
-				ph_enable(devptr);
-			}else{
-				ph_disable(devptr);
-			}
-
+			printf("Habilitando stepper:\n");
+			stp_enable(devptr);
+	
+		}else if(!strcmp(line,"disable\n")){
+			printf("Deshabilitando stepper:\n");
+			
+			stp_disable(devptr);
+	
 		}else if(!strcmp(line,"exit\n")){
 			printf("Cerrando el programa. Adiós\n");
 			break;
 		}else{
-			printf("Comando inválido. Utilice una de las siguientes opciones:\n" 
-				"\t enable\t\t\t-Habilita o deshabilita el puente H. Valores de entrada 1 o 0\n"
-				"\t medir-velocidad\t-Imprime la velocidad actual del motor\n "
-				"\t medir-temperatura\t-Imprime la temperatura actual del motor\n "
-				"\t medir-corriente\t-Imprime la corriente actual que atravieza el motor\n"
-				"\t dar-estado\t \t-Imprime el estado de operación actual del motor\n"
-				"\t cambiar-pwm\t\t-Cambia ciclo útil del PWM. Valor de entrada entre 0 y 255\n"
-				"\t cambiar-direccion\t-Cambia la dirección de giro del motor. Valor de entrada 1 o 0\n"
-				"\t cambiar-velocidad\t-Cambia la velocidad objetivo del controlador. Valor de entrada entre ------\n"
-				"\t cambiar-estado\t \t-Cambia el estado de operación del puente H. VAlor de entrada entre 16 o 32 TODO: Parser\n"
-				"\t cambiar-kic\t\t-Cambia el valor de KIC. Valor de entrada entre 0 y 255\n"
-				"\t cambiar-kpc\t\t-Cambia el valor de KPC. Valor de entrada entre 0 y 255\n"
-				"\t cambiar-kiv\t\t-Cambia el valor de KIV. Valor de entrada entre 0 y 255\n"
-				"\t cambiar-kpv\t\t-Cambia el valor de KPV. Valor de entrada entre 0 y 255\n"
-				"\t debug\t\t-Permite rápido envia de CAMBIAR_PWM con un valor de 0x5A\n");
+		printf("Bienvenido al test de funcionamiento de Puente H (ERC 2015-ROBOCOL).\n Utilice una de los siguientes comandos:\n" 
+				"\t enable\t\t\t-Habilita el stepper\n"
+				"\t disable\t\t\t-Deshabilita el stepper\n"
+				"\t stat\t\t\t\t-Imprime el STATUS register\n "
+				"\t get-config\t\t\t-Imprime el CONFIG register\n "
+				"\t get-step\t\t\t-Imprime el estado actual del selector de paso\n"
+				"\t get-alarm\t\t\t-Imprime el registro de alarmas\n"
+				"\t get-ocdt\t\t\t-Imprime el Overcurrent Detection Threshold actual\n"
+				"\t get-tval\t\t\t-Imprime el registro de alarmas\n"
+				"\t period\t\t\t-Imprime el registro de alarmas\n"
+				"\t set-config\t\t\t-Imprime el CONFIG register\n "
+				"\t set-step\t\t\t-Imprime el estado actual del selector de paso\n"
+				"\t set-alarm\t\t\t-Imprime el registro de alarmas\n"
+				"\t set-ocdt\t\t\t-Imprime el Overcurrent Detection Threshold actual\n"
+				"\t set-tval\t\t\t-Imprime el registro de alarmas\n"
+				"\t debug\t\t\t-Permite rápido envia de CAMBIAR_PWM con un valor de 0x5A\n");
+		
 		}
 		printf("--------------------------------------------------------------\n");
 	}
