121017683fb12548662e5ffe93ac3e01a33d97ba
gf.giraldo239@uniandes.edu.co <german@german.(none)>
2015-05-23 17:09:00 -0500

    Avance proyecto café. Actualización Makefiles

--- Code/Galileo/Perifericos/TEMPERATURA/lmp_cafe.c
+++ Code/Galileo/Perifericos/TEMPERATURA/lmp_cafe.c
@@ -9,8 +9,10 @@
 #include <stdio.h>
 #include "spi_robocol.h"
 #include "misc_robocol.h"
+#include "lmp_robocol.h"
 
 const uint32_t 	RESOLUTION=8388608;
+const uint8_t 	NUM_AFE=14;
 const float		VREF=5;
 const float		THRESHOLD=50;
 
@@ -66,7 +68,7 @@ int main(){
 
 	int i=0
 	for (i=0;i<14;i++){
-		build_lmp(lmp[i]);
+		build_lmp(&(lmp[i]));
 	}
 
 
@@ -75,38 +77,17 @@ int main(){
 	printf("\n--------------------------------------------------------------------------------------\n");
 
 
-	
-	//D6 as Output
-	tx[0]=0x10;
-	tx[1]=0x00; //Upper Address
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en set de upper Address para set de D6DIR\n");
-		return 1;
-
+	/*Encendiendo Leds (Si es que no se queman)*/
+	for (i=0;i<NUM_AFE;i++){
+		if(write_reg_lmp(&(lmp[i]),0x0E, 0x41,1)){
+			printf("Error configurando dirección de GPIOs de la AFE %d\n",i);
+		}
 	}
-	tx[0]=0x8E; //Lower Address
-	tx[1]=0x20; 
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en set de upper Address para set D6\n");
-		return 1;
-
+	for (i=0;i<NUM_AFE;i++){
+		if(write_reg_lmp(&(lmp[i]),0x0F, 0x01,1)){
+			printf("Error configurando asignando valor de GPIOs de la AFE %d\n",i);
+		}
 	}
-
-	//D6 
-	tx[0]=0x10;
-	tx[1]=0x00; //Upper Address
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en set de upper Address para Configuración de Background Calibration\n");
-		return 1;
-	}
-	tx[0]=0x8F; //
-	tx[1]=0x00;
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en set de upper Address para Configuración de Background Calibration\n");
-		return 1;
-
-	}
-
 
 
 	/*Configuración de Mediciones*/
@@ -114,169 +95,22 @@ int main(){
 	printf("Configurando mediciones\n\n");
 
 	//Background Calibration
-	tx[0]=0x10;
-	tx[1]=0x01; //Upper Address
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en set de upper Address para Configuración de Background Calibration\n");
-		return 1;
+	for (i=0;i<NUM_AFE;i++){
+		 //Modo 2:offset correction/gain correction
+		if(write_reg_lmp(&(lmp[i]),0x10, 0x02,1)){
+			printf("Error configurando asignando valor de GPIOs de la AFE %d\n",i);
+		}
 	}
 
-	printf("Background Calibration Configuration: ");
-	tx[0]=0x00;
-	tx[1]=0x02; //Modo 2:offset correction/gain correction 
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en transacción con lower Address para Background Calibration\n");
-		return 1;
+	// Detección y Configuración de Clk. Configuración de fuentes de corriente
+	printf("Detección y Configuración de Clk y fuentes de corriente:");
+	for (i=0;i<NUM_AFE;i++){
+		//Deshabilitada detección de CLK - Seleccionado EXCLK -Corriente 1mA
+		if(write_reg_lmp(&(lmp[i]),0x12, 0x3A,1)){
+			printf("Error configurando detección de clock y fuentes de corriente de la AFE %d\n",i);
+		}
 	}
 
-	// Lectura de registro escrito
-	tx[0]=0x80;
-	tx[1]=0x00; //Modo 2:offset correction/gain correction 
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en transacción con lower Address para Background Calibration\n");
-		return 1;
-	}
-
-	if(rx[1]==0x02){
-		printf("Done. LMP90100 Online\n" );
-	}else{
-		printf("Failed\n" );
-	}
-
-// Configuración interrupción DRDY
-	// printf("Configuración Interrupción Data ready:");
-	// //Interrupción por nuevo dato disponible
-	// tx[0]=0x01; //Lower Address
-	// tx[1]=0x80; //Activación de interrupción
-	// if(spi_rw(&dev,tx,rx,2)){
-	// 	printf("Error en set para interrupción de Data ready\n");
-	// 	return 1;
-	// }
-
-	// // Lectura de registro escrito
-	// tx[0]=0x81; //Lower Address
-	// tx[1]=0x00; //Activación de interrupción
-	// if(spi_rw(&dev,tx,rx,2)){
-	// 	printf("Error en set para interrupción de Data ready\n");
-	// 	return 1;
-	// }
-	// if(rx[1]==0x80){
-	// 	printf("Done\n" );
-	// }else{
-	// 	printf("Failed\n" );
-	// }
-
-// Detección y Configuración de Clk Lower Address
-	printf("Detección y Configuración de Clk Lower Address:");
-	//Output Data Rate
-	tx[0]=0x02; //Lower Address
-	tx[1]=0x3A; //Deshabilitada detección de CLK - Seleccionado EXCLK -Corriente 1mA
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en transacción lower Address Configuración de Clk\n");
-		return 1;
-	}
-
-
-	// Lectura de registro escrito
-	tx[0]=0x82; //Lower Address
-	tx[1]=0x00; 
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en set para interrupción de Data ready\n");
-		return 1;
-	}
-	if(rx[1]==0x3A){
-		printf("Done\n" );
-	}else{
-		printf("Failed\n" );
-	};
-
-
-	//Configuración de Channel 0
-	tx[0]=0x10;
-	tx[1]=0x02; //Upper Address
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en transacción para Configuración de Upper Address\n");
-		return 1;
-	}
-
-
-	printf("Configuración de Channel 0\n");
-	tx[0]=0x01; //Lower Address
-	tx[1]=0x30; //Gain = 1 - Buffer enabled
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en transacción para Configuración de Channel 0.\n");
-		return 1;
-	}
-
-	//Lectura registro escrito
-	tx[0]=0x81;
-	tx[1]=0x00;
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en transacción de lectura\n");
-		return 1;
-	}
-	if(rx[1]==0x30){
-		printf("Done\n" );
-
-	}else{
-		printf("Failed\n" );
-
-	};
-
-
-//Configuración de Channel 1
-	printf("Configuración de Channel 1\n");
-	tx[0]=0x02; //Lower Address
-	tx[1]=0x1C; //VINP=VIN3; VINN=VIN4;
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en transacción para Configuración de Channel 1.\n");
-		return 1;
-	}
-
-	//Lectura registro escrito
-	tx[0]=0x82;
-	tx[1]=0x00;
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en transacción de lectura\n");
-		return 1;
-	}
-	if(rx[1]==0x1C){
-		printf("Done\n" );
-
-	}else{
-		printf("Failed\n" );
-
-	};
-
-	tx[0]=0x03; //Lower Address
-	tx[1]=0x30; //Gain = 1 - Buffer enabled
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en transacción para Configuración de Channel 1.\n");
-		return 1;
-	}
-
-	//Lectura registro escrito
-	tx[0]=0x83;
-	tx[1]=0x00;
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en transacción de lectura\n");
-		return 1;
-	}
-	if(rx[1]==0x30){
-		printf("Done\n" );
-
-	}else{
-		printf("Failed\n" );
-
-	};
-
-
-	tx[0]=0x10;
-	tx[1]=0x01; //Upper Address
-	if(spi_rw(&dev,tx,rx,2)){
-		printf("Error en transacción para Configuración de Upper Address\n");
-		return 1;
-	}
 
 
 	printf("Bienvenido al test de medición de dos canales por medio de LMP90100 (Intrumentación Electrónica 201510).\n Utilice una de los siguientes comandos:\n" 
@@ -288,118 +122,58 @@ int main(){
 
 	/*----------Mediciones iterativas----------*/
 
-	while((entrada=getchar()) != 'q'){
-		if(entrada=='0'){
+	while(){
+		for(i=0;i<NUM_AFE;i++){
 
-			printf("Configurando Medición CH0.\n");
+			printf("Mediciónes de AFE %d.\n",i);
+			if(i<12){
 
-			//Configuración de Channel 0
-			tx[0]=0x0F; //Lower Address
-			tx[1]=0x00; //ScanMode1 - Single Scan CH0
-			if(spi_rw(&dev,tx,rx,2)){
-				printf("Error en transacción para Configuración de Channel 0.\n");
-				return 1;
-			}
+				//Configuración de Channel 0
+				if(write_reg_lmp(&(lmp[i]),0x1F, 0x00,1)){
+					printf("Error configurando detección de clock y fuentes de corriente de la AFE %d\n",i);
+				}
+				if(read_ADC_lmp(lmp[i],rxADC)){
+					printf("Error medición t%d.\n",i);
+				}
+				T[2*i]=rxADC*VREF/RESOLUTION;
 
-			//Lectura registro escrito
-			tx[0]=0x8F;
-			tx[1]=0x00;
-			if(spi_rw(&dev,tx,rx,2)){
-				printf("Error en transacción de lectura\n");
-				return 1;
-			}
-			if(rx[1]==0x00){
-				printf("Done\n" );
-
-			}else{
-				printf("Failed\n" );
-
-			};
-
-			printf("Espere un monento...\n");
-			sleep(3);
-
-			txADC[0]=0xCA;
-			txADC[1]=0x00;
-			txADC[2]=0x00;
-			txADC[3]=0x00;
-			if(spi_rw(&dev,txADC,rxADC,3)){
-				printf("Error en transacción para lectura de ADC\n");
-			}
+				//Configuración de Channel 0
+				if(write_reg_lmp(&(lmp[i]),0x1F, 0x09,1)){
+					printf("Error configurando detección de clock y fuentes de corriente de la AFE %d\n",i);
+				}
+				if(read_ADC_lmp(lmp[i],rxADC)){
+					printf("Error medición t%d.\n",i);
+				}
+				T[2*i+1]=rxADC*VREF/RESOLUTION;
 
 
-			//Checking for negatives
-			if((rxADC[1]&0x80)){
-				rxADC[0]=0xFF;
-			}else{
-				rxADC[0]=0x00;
-			}
-
-			adc=((float)(array_to_i32(rxADC,4,0))*VREF)/(RESOLUTION);
-			printf("RAW: %d\t",array_to_i32(rxADC,4,0));
-			printf("ADC[Volts]: %.4f\t\n",adc);
-
-
-		}else if(entrada=='1'){
-
-
-			printf("Configurando Medición CH1.\n");
-			//Configuración de Channel 1
-			tx[0]=0x0F; //Lower Address
-			tx[1]=0x09; //ScanMode1 - Single Scan CH1
-			if(spi_rw(&dev,tx,rx,2)){
-				printf("Error en transacción para Configuración de Channel 1.\n");
-				return 1;
-			}
-
-			//Lectura registro escrito
-			tx[0]=0x8F;
-			tx[1]=0x0F;
-			if(spi_rw(&dev,tx,rx,2)){
-				printf("Error en transacción de lectura\n");
-				return 1;
-			}
-			if(rx[1]==0x09){
-				printf("Done\n" );
+				//Configuración de Channel 0
+				if(write_reg_lmp(&(lmp[i]),0x1F, 0x00,1)){
+					printf("Error configurando detección de clock y fuentes de corriente de la AFE %d\n",i);
+				}
+				if(read_ADC_lmp(lmp[i],rxADC)){
+					printf("Error medición t%d.\n",i);
+				}
+				U[i]=rxADC*VREF/RESOLUTION;
 
 			}else{
-				printf("Failed\n" );
 
-			};
+				if(read_ADC_lmp(lmp[i],rxADC)){
+					printf("Error medición t%d.\n",i);
+				}
+				T[2*i]=rxADC*VREF/RESOLUTION;
 
-			printf("Espere un monento...\n");
+				if(read_ADC_lmp(lmp[i],rxADC)){
+					printf("Error medición t%d.\n",i);
+				}
+				T[2*i+1]=rxADC*VREF/RESOLUTION;
 
-			sleep(3);
-
-			txADC[0]=0xCA;
-			txADC[1]=0x00;
-			txADC[2]=0x00;
-			txADC[3]=0x00;
-			if(spi_rw(&dev,txADC,rxADC,3)){
-				printf("Error en transacción para lectura de ADC\n");
-				abort();
+				if(read_ADC_lmp(lmp[i],rxADC)){
+					printf("Error medición t%d.\n",i);
+				}
+				P[i-12]=rxADC*VREF/RESOLUTION;
 			}
-
-			//Checking for negatives
-			if((rxADC[1]&0x80)){
-				rxADC[0]=0xFF;
-			}else{
-				rxADC[0]=0x00;
-			}
-
-			raw=array_to_i32(rxADC,4,0);
-
-			adc=((float)raw*VREF)/(RESOLUTION);
-	
-			printf("RAW: %d\t",raw);
-			printf("ADC[Volts]: %.4f\t\n",adc);
-
-
-		}else if(entrada!='\n') {
-		printf("Comando inválido.Por favor utilice una de los siguientes comandos:\n" 
-		"\t 0\t\t-Realiza una única lectura del canal de medición 0 del AFE (VIN0-VIN1)\n"
-		"\t 1\t\t-Realiza una única lectura del canal de medición 0 del AFE (VIN2-VIN3)\n"
-		"\t q\t\t-Izquierda\n\n");
 		}
 	}
+
 }
