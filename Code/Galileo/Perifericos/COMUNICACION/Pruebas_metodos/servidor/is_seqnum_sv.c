96c39905c421390d2ce16e9de5a69fbb9eab8ebc
Robocol <robocol@robocol-TOSHIBA-NB255.(none)>
2015-06-13 17:26:55 -0500

    Servidor  con metodos para volverse demonio y con metodos de log

--- Code/Galileo/Perifericos/COMUNICACION/Pruebas_metodos/servidor/is_seqnum_sv.c
+++ Code/Galileo/Perifericos/COMUNICACION/Pruebas_metodos/servidor/is_seqnum_sv.c
@@ -48,8 +48,10 @@
 #include <limits.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <syslog.h>
 
-#include <linux/videodev2.h>
+#include <stdarg.h>
+
 #define CLEAR(x) memset(&(x), 0, sizeof(x))
 
 #define PORT_NUM "50002"        /* Port number for server */
@@ -71,9 +73,24 @@
 	#define false 0
 #endif
 
+/* Bit-mask values for 'flags' argument of becomeDaemon() */
+
+#define BD_NO_CHDIR           01    /* Don't chdir("/") */
+#define BD_NO_CLOSE_FILES     02    /* Don't close all open files */
+#define BD_NO_REOPEN_STD_FDS  04    /* Don't reopen stdin, stdout, and
+                                       stderr to /dev/null */
+#define BD_NO_UMASK0         010    /* Don't do a umask(0) */
+
+#define BD_MAX_CLOSE  8192          /* Maximum file descriptors to close if
+                                       sysconf(_SC_OPEN_MAX) is indeterminate */
+
+
 //Variables globales*************************************************************
 bool archivo1=false;//Indica si el archivo 1 se modifico
 bool archivo2=false;//Indica si el archivo 2 se modifico
+static FILE *logfp;                 /* Log file stream */
+
+static const char *LOG_FILE = "/tmp/log_servidor.log";
 
 struct buffer {
         void   *start;
@@ -112,6 +129,7 @@ ssize_t readLine(int fd, void *buffer, size_t n){
 
 	totRead = 0;
 	for (;;) {
+
 	        numRead = read(fd, &ch, 1);
 
 		if (numRead == -1) {
@@ -141,43 +159,126 @@ ssize_t readLine(int fd, void *buffer, size_t n){
 	return totRead;
 }
 
+
+int becomeDaemon(int flags)
+{
+    int maxfd, fd;
+
+    switch (fork()) {                   /* Become background process */
+    case -1: return -1;
+    case 0:  break;                     /* Child falls through... */
+    default: _exit(EXIT_SUCCESS);       /* while parent terminates */
+    }
+
+    if (setsid() == -1)                 /* Become leader of new session */
+        return -1;
+
+    switch (fork()) {                   /* Ensure we are not session leader */
+    case -1: return -1;
+    case 0:  break;
+    default: _exit(EXIT_SUCCESS);
+    }
+
+    if (!(flags & BD_NO_UMASK0))
+        umask(0);                       /* Clear file mode creation mask */
+
+    if (!(flags & BD_NO_CHDIR))
+        chdir("/");                     /* Change to root directory */
+
+    if (!(flags & BD_NO_CLOSE_FILES)) { /* Close all open files */
+        maxfd = sysconf(_SC_OPEN_MAX);
+        if (maxfd == -1)                /* Limit is indeterminate... */
+            maxfd = BD_MAX_CLOSE;       /* so take a guess */
+
+        for (fd = 0; fd < maxfd; fd++)
+            close(fd);
+    }
+
+    if (!(flags & BD_NO_REOPEN_STD_FDS)) {
+        close(STDIN_FILENO);            /* Reopen standard fd's to /dev/null */
+
+        fd = open("/dev/null", O_RDWR);
+
+        if (fd != STDIN_FILENO)         /* 'fd' should be 0 */
+            return -1;
+        if (dup2(STDIN_FILENO, STDOUT_FILENO) != STDOUT_FILENO)
+            return -1;
+        if (dup2(STDIN_FILENO, STDERR_FILENO) != STDERR_FILENO)
+            return -1;
+    }
+
+    return 0;
+}
+
+static void logMessage(const char *format, ...)
+{
+    va_list argList;
+    const char *TIMESTAMP_FMT = "%F %X";        /* = YYYY-MM-DD HH:MM:SS */
+#define TS_BUF_SIZE sizeof("YYYY-MM-DD HH:MM:SS")       /* Includes '\0' */
+    char timestamp[TS_BUF_SIZE];
+    time_t t;
+    struct tm *loc;
+
+    t = time(NULL);
+    loc = localtime(&t);
+    if (loc == NULL ||
+           strftime(timestamp, TS_BUF_SIZE, TIMESTAMP_FMT, loc) == 0)
+        fprintf(logfp, "???Unknown time????: ");
+    else
+        fprintf(logfp, "%s: ", timestamp);
+
+    va_start(argList, format);
+    vfprintf(logfp, format, argList);
+    fprintf(logfp, "\n");
+    va_end(argList);
+}
+
+/* Open the log file 'logFilename' */
+
+static void logOpen(const char *logFilename)
+{
+    mode_t m;
+
+    m = umask(077);
+    logfp = fopen(logFilename, "a");
+    umask(m);
+
+    /* If opening the log fails we can't display a message... */
+
+    if (logfp == NULL)
+        exit(EXIT_FAILURE);
+
+    setbuf(logfp, NULL);                    /* Disable stdio buffering */
+
+    logMessage("Opened log file");
+}
+
+/* Close the log file */
+
+static void logClose(void)
+{
+    logMessage("Closing log file");
+    fclose(logfp);
+}
+
 int main(int argc, char *argv[]) {
-//Variables locales servidor
-	char reqLenStr[INT_LEN];            /* Length of requested sequence */
-	struct sockaddr_storage claddr;
+
+	//Variables locales servidor
+	char reqLenStr[INT_LEN];	/* Buffer para almacenar lo que llega de un cliente */
+	struct sockaddr_storage claddr;	/* Buffer para almacenar lo que llegs del cliente */	
 	int lfd, cfd, optval;//
 	socklen_t addrlen;
-	struct addrinfo hints; //
+	struct addrinfo hints; // Estructura de informacion del socket y la direccion
 	struct addrinfo *result, *rp;//
 	#define ADDRSTRLEN (NI_MAXHOST + NI_MAXSERV + 10)
-	char addrStr[ADDRSTRLEN];
-	char host[NI_MAXHOST];
-	char service[NI_MAXSERV];
 	ssize_t escritos;
-	char str[15];
-	bool inicio=true;
-	struct tm* clock1;				// Crea una estructura de tiempo
-	struct stat attrib1;			// crea una estrusctura de estadisticas	
-	
-	struct tm* clock2;				// Crea una estructura de tiempo
-	struct stat attrib2;			// crea una estrusctura de estadisticas	
-	
-	struct tm* clock3;				// Crea una estructura de tiempo
-	struct stat attrib3;			// crea una estrusctura de estadisticas	
-	
-	struct tm* clock4;				// Crea una estructura de tiempo
-	struct stat attrib4;			// crea una estrusctura de estadisticas	
-	
-	struct tm* clock0;				// Crea una estructura de tiempo
-	struct tm* clock;				// Crea una estructura de tiempo
+	logOpen(LOG_FILE);
+
+	if(becomeDaemon(0) == -1)
+		logMessage("Fail becomeDaemon");
 
 
-//Variables para notificacion
-	int fd, sz;
-	char* frame="2";
-	char* imagen_rec;
-	long tiempo;
-	long tiempo_ant=0;
+
 	
 //Verifica los parametros ingresados por terminal
 	if (argc > 2 && strcmp(argv[1], "--help") == 0){
@@ -190,7 +291,7 @@ int main(int argc, char *argv[]) {
 	/* Ignore the SIGPIPE signal, so that we find out about broken connection
 	errors via a failure from write(). */
 	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR){
-	        fprintf(stderr,"signal error");
+	        logMessage("Fail ignoring SIGPIPE ");
 		exit(EXIT_FAILURE);
 	}
 
@@ -205,8 +306,9 @@ int main(int argc, char *argv[]) {
 	hints.ai_flags = AI_PASSIVE | AI_NUMERICSERV;
                        /* Wildcard IP address; service name is numeric */
 
+	// Identifica la informacion de las direcciones IP del computador
 	if (getaddrinfo(NULL, PORT_NUM, &hints, &result) != 0){
-		fprintf(stderr,"getaddrinfo error");
+		logMessage("Fail getting address info (%s)", strerror(errno));
 		exit(3);
 	}
 
@@ -219,7 +321,7 @@ int main(int argc, char *argv[]) {
 	            continue;                   /* On error, try next address */
 
 	        if (setsockopt(lfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval))== -1){		
-			fprintf(stderr,"setsockopt error");
+			logMessage("Fail setting socket options (%s)", strerror(errno));
 			exit(4);
 		}
 
@@ -232,14 +334,16 @@ int main(int argc, char *argv[]) {
 	}
 
 	if (rp == NULL){
-		fprintf(stderr,"Could not bind socket to any address");
+		logMessage("Could not bind to any socket (%s)", strerror(errno));
 		exit(5);
 	}
 
 	if (listen(lfd, BACKLOG) == -1){
-		fprintf(stderr,"listen error");
+		logMessage("Fail listening (%s)", strerror(errno));
 		exit(6);
 	}
+	printf("Configura el servidor");
+	logMessage("Configura el servidor");
 
 	freeaddrinfo(result);
 
@@ -251,18 +355,11 @@ int main(int argc, char *argv[]) {
         	addrlen = sizeof(struct sockaddr_storage);
         	cfd = accept(lfd, (struct sockaddr *) &claddr, &addrlen);
         	if (cfd == -1) {
-			fprintf(stderr,"accept error");
+			logMessage("Accept error (%s)", strerror(errno));
 			continue;
         	}
-		printf("6. Acepto cliente\n");
-/*
-	        if (getnameinfo((struct sockaddr *) &claddr, addrlen,host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
-		        snprintf(addrStr, ADDRSTRLEN, "(%s, %s)", host, service);
-	        else
-        		snprintf(addrStr, ADDRSTRLEN, "(?UNKNOWN?)");
-        		printf("Connection from %s\n", addrStr);
+		logMessage("acepto cliente ");
 
-*/
 		printf("7. Adquirio la información del cliente\n");
 
 		/* Lee la peticion del cliente y envia una respuesta o si no cierra el socket 
@@ -270,7 +367,7 @@ int main(int argc, char *argv[]) {
 
 	        if (readLine(cfd, reqLenStr, INT_LEN) <= 0) {
 			close(cfd);
-			printf("No hay fin de línea\n");	
+			logMessage("No hay fin de línea\n");	
 			continue;                   
         	}
 
@@ -278,192 +375,51 @@ int main(int argc, char *argv[]) {
 
 		/* Si hay una peticion de imagen adquiere y envia la imagen */
 		if(strcmp(reqLenStr,"imagen\n")==0){
-			printf("Ingreso al método para adquirir la imagen\n");
+			logMessage("Ingreso al método para adquirir la imagen\n");
 			
-			printf("Leyo el estado:\n");
-			printf(frame);
-			printf("\n");
+			logMessage("Leyo el estado:\n");
+			
+			logMessage("\n");
 
-			/* Se verifica cual imagen es mas reciente */
+			/* Se verifica cual imagen es mas reciente 
 			stat("./imagen1.mp4", &attrib1);	// get the attributes of afile.txt
 			clock1 = localtime(&(attrib1.st_ctime));	// Get the last modified time
 			tiempo=attrib1.st_mtim.tv_nsec; 
 			imagen_rec="imagen1.mp4";
-/*			stat("./imagen2.mp4", &attrib2);	// get the attributes of afile.txt
-			clock2 = localtime(&(attrib2.st_ctime));	// Get the last modified time
-			printf("mtime = %lld.%.9ld\n",(long long)attrib2.st_mtim.tv_sec, attrib1.st_mtim.tv_nsec);
-			stat("./imagen3.mp4", &attrib3);	// get the attributes of afile.txt
-			clock3 = localtime(&(attrib3.st_ctime));	// Get the last modified time 
-			printf("mtime = %lld.%.9ld\n",(long long)attrib3.st_mtim.tv_sec, attrib1.st_mtim.tv_nsec);
-			stat("./imagen4.mp4", &attrib4);	// get the attributes of afile.txt
-			clock4 = localtime(&(attrib4.st_ctime));	// Get the last modified time
-			printf("mtime = %lld.%.9ld\n",(long long)attrib4.st_mtim.tv_sec, attrib1.st_mtim.tv_nsec);
 
-			printf("sec 1:%i \n",clock1->tm_sec);
-			printf("sec 2:%i \n",clock2->tm_sec);
-			printf("sec 3:%i \n",clock3->tm_sec);
-			printf("sec 4:%i \n",clock4->tm_sec);
-
-			printf("Obtuvo estadisiticas de los archivos\n");
-
-			if(clock1->tm_sec > clock2->tm_sec){
-				printf("1>2\n");
-				if(clock1->tm_sec > clock3->tm_sec){
-					printf("1>3\n");
-					if(clock1->tm_sec >= clock4->tm_sec){
-						printf("1>=4\n");
-						imagen_rec="imagen1.mp4";
-						clock=clock1;
-						if(inicio){
-							clock0=clock2;
-							inicio=false;
-						}
-					}else{
-						printf("4>1\n");
-						imagen_rec="imagen4.mp4";
-						clock=clock4;
-						if(inicio){
-							clock0=clock2;
-							inicio=false;
-						}
-					}
-				}else{
-					printf("3>=1\n");					
-					if(clock3->tm_sec > clock4->tm_sec){
-						printf("3>4\n");
-						imagen_rec="imagen3.mp4";
-						clock=clock3;
-						if(inicio){
-							clock0=clock2;
-							inicio=false;
-						}
-					}else{
-						printf("4>=3\n");					
-						imagen_rec="imagen4.mp4";
-						clock=clock4;
-						if(inicio){
-							clock0=clock2;
-							inicio=false;
-						}
-					}
-				}	
-			}else{
-				printf("2>=1\n");				
-				if(clock2->tm_sec > clock3->tm_sec){
-					printf("2>3\n");
-					if(clock2->tm_sec > clock4->tm_sec){
-						printf("2>4\n");
-						imagen_rec="imagen2.mp4";
-						clock=clock2;
-						if(inicio){
-							clock0=clock3;
-							inicio=false;
-						}
-					}else{
-						printf("4>=2\n");
-						imagen_rec="imagen4.mp4";
-						clock=clock4;
-						if(inicio){
-							clock0=clock3;
-							inicio=false;
-						}						
-					}
-				}else{
-					printf("3>=2\n");
-					if(clock3->tm_sec > clock4->tm_sec){
-						printf("3>4\n");					
-						imagen_rec="imagen3.mp4";
-						clock=clock3;
-						if(inicio){
-							clock0=clock4;
-							inicio=false;
-						}
-					}else{
-						printf("4>=3\n");					
-						imagen_rec="imagen4.mp4";
-						clock=clock4;
-						if(inicio){
-							clock0=clock2;
-							inicio=false;
-						}
-					}
-				}		
-			}	
 			
-			printf("Archivo mas reciente:");
-			printf(imagen_rec);
-			printf("\n");
-*/			
 			
 
-			/* Verifica si el archivo no se ha enviado antes */
-			if(tiempo!=tiempo_ant){
-				printf("El archivo no se envio antes\n");				
-				tiempo_ant=tiempo;
-				printf("Comparo frame\n");
-				/* Abre el archivo de la imagen */				
-				fd=open(imagen_rec,O_RDONLY);				
-				if(fd==-1){
-					fprintf(stderr,"No se pudo abrir el archivo de la imagen");
-					exit(9);
-				}
-				printf("Archivo abierto\n");
-				
-				sz=fsize(imagen_rec);				
-				
-				printf("tamanho = %i", sz);				
-
-				/* Envia el tamanho de la imagen */
-				sprintf(str, "%d", sz);
-				escritos = write(cfd,str,15);
-				if(escritos==-1){
-					fprintf(stderr,"No se pudo escribir en el socket");
-					exit(9);
-				}
-
-				escritos = write(cfd,"\n",1);		
-				if(escritos==-1){
-					fprintf(stderr,"No se pudo escribir en el socket");
-					exit(9);
-				}
-
-				// Lee una linea de respuesta  
-	        		if (readLine(cfd, reqLenStr, INT_LEN) <= 0) {
-        				close(cfd);
-     		    			printf("No hay fin de línea\n");	
-					continue;                   
-        			}
-
-				printf(reqLenStr);
-
-				/* Compara la respuesta y envia la imagen */
-				if(strcmp(reqLenStr,"tamanho\n")==0){
-					escritos = sendfile(cfd, fd, NULL, sz);
-	        			if (escritos == -1){
-						fprintf(stderr,"No se pudo escribir en el socket");
-						exit(9);
-					}
-				}
-
-				/* Cierra el archivo de la imagen */
-				close(fd);				
-		
-			}else{
-				printf("No hay una imagen nueva\n");				
-				escritos = write(cfd,"No hay imagen\n",14);		
-				if(escritos==-1){
-					fprintf(stderr,"No se pudo escribir en el socket");
-					exit(9);
-				}
+			 Envia el tamanho de la imagen */
+			//sprintf(str, "%d", sz);
+			escritos = write(cfd,"good",15);
+			if(escritos==-1){
+				logMessage("Failing writing socket (%s)", strerror(errno));
+				exit(9);
 			}
+
+			escritos = write(cfd,"\n",1);		
+			if(escritos==-1){
+				logMessage("Failing writing socket (%s)", strerror(errno));
+				exit(9);
+			}
+
+			// Lee una linea de respuesta  
+        		if (readLine(cfd, reqLenStr, INT_LEN) <= 0) {
+       				close(cfd);
+	    			logMessage("No hay fin de linea (%s)", strerror(errno));	
+				continue;                   
+       			}
+
+			logMessage(reqLenStr);
 						
-			
+		
         	}		 
 
         	if (close(cfd) == -1)           /* Close connection */
-			fprintf(stderr,"close error");		
+			logMessage("Close error (%s)", strerror(errno));	
 		
-		printf("Finalizo rutina\n");
+		logMessage("Finalizo rutina\n");
     	}
 
 }
