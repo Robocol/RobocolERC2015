d40740f9180a91cc7b4c81b6c50bc319c25df9be
Haldaron <gf.giraldo239@uniandes.edu.co>
2015-07-23 14:00:54 -0500

    Cambios menores a prueba uart. Creacion de makefile para la libreria UART

--- Code/Galileo/Perifericos/UART/uart_test.c
+++ Code/Galileo/Perifericos/UART/uart_test.c
@@ -1,9 +1,10 @@
 #include "uart_robocol.h"
 #define BAUDRATE B115200
+#define BUFF_SIZE 2
 
 int main(int argc, char const *argv[]){
-	char buf_tx[2];
-	char buf_rx[2];
+	char buf_tx[BUFF_SIZE];
+	char buf_rx[BUFF_SIZE];
 	struct termios newtio;
 	newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD ;
 	newtio.c_iflag = ICRNL;
@@ -11,17 +12,28 @@ int main(int argc, char const *argv[]){
 	newtio.c_lflag = 0;
 	newtio.c_cc[VMIN]=1;
 	newtio.c_cc[VTIME]=0;
+
 	uart_open("/dev/ttyS0",&newtio);
+
 	while (1) {
-		fgets(buf_tx, 2, stdin);
-		uart_write(&buf_tx, 2);
-		usleep(1000);
-		if (getWaitFlag()==FALSE ) {
-			uart_read(&buf_rx,2);
-			printf("%s",buf_rx);
+
+		printf("Escriba un mensaje:\n");
+		fgets(buf_tx,BUFF_SIZE,stdin);
+		printf("%s\n", buf_tx);
+		uart_write(buf_tx, BUFF_SIZE);
+
+		if (getWaitFlag()==FALSE ){
+			printf("Recibido:\n");
+			usleep(100000);
+			uart_read(buf_rx,BUFF_SIZE);
+			buf_rx[BUFF_SIZE-1]='\0';
+			printf("E: %s",buf_rx);
 		}
+
 		setWaitFlag(TRUE);
 	}
+
 	uart_close();
+
 }
 
